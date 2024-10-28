b16b0d390f94aa3371cd87cf9b910e05435398e4
BartCatalano <catalanobartolomeo@gmail.com>
2024-10-28 16:30:41 +0100

    Feat: costo del biglietto calcolato, inserimento Condizionali per sconti (wip)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-alert("ciao")
+// Input utente
+// chiediamo ad utente km da percorrere ed età
+
+// Prompt età
+const userAge = prompt("Inserisci la tua età");
+
+console.log(userAge, typeof userAge)
+
+// Controlliamo se il dato inserito è realmente un numero
+if (isNaN(userAge)){
+    alert("Inserire età in maniera corretta!")
+}
+
+// Convertiamo la stringa età in numero
+const userVAge = parseInt(userAge);
+     //  userVerificatedAge
+console.log(userVAge); 
+
+
+// Prompt Km
+const userKm = prompt("Inserisci i KM da percorrere");
+
+console.log(userKm, typeof userKm);
+
+// Controlliamo se il dato inserito è realmente un numero
+if (isNaN(userKm)){
+    alert("Inserire Km in maniera corretta!")
+}
+
+// Convertiamo la stringa Km in numero
+const userVKm = parseInt(userKm);
+     //  userVerificatedKm
+console.log(userVKm); 
+
+
+// Calcoliamo costo del biglietto
+
+const TicketValue = (userVKm * 0.21);
+console.log(TicketValue);
+
+// Controlliamo se bisogna applicare uno sconto
+
